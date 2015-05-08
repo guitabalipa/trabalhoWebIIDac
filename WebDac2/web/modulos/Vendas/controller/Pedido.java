4d09926548a061262430dc9aa7951fc0c11abc20
Guilherme <guitabalipa@gmail.com>
2015-05-08 18:21:02 -0300

    algumas classes(Pedido, Produto e StatusPedido

---
+++ WebDac2/web/modulos/Vendas/controller/Pedido.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modulos.Vendas.controller;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Pedido {
+    
+    private int id;
+    private Double valorTotal;
+    private String observacoes;
+    private String data;
+
+    public Pedido(Double valorTotal, String data) {
+        this.valorTotal = valorTotal;
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+    
+}
---
+++ WebDac2/web/modulos/Vendas/controller/Produto.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modulos.Vendas.controller;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Produto {
+    
+    private int id;
+    private String nome;
+    private String descricao;
+    private String marca;
+    private int categoria;
+    private double valor;
+    private int quantidadeEmEstoque;
+
+    public Produto(String nome, String marca, double valor, int quantidadeEmEstoque) {
+        this.nome = nome;
+        this.marca = marca;
+        this.valor = valor;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+    
+}
